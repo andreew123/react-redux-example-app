84a97c39d492df0e9b1dc4304cb51801c927a5ac
Adorján András <stellarkartfan@gmail.com>
2017-02-09 22:15:01 +0100

    Update readme, add db backup

--- README.md
+++ README.md
@@ -4,13 +4,17 @@
 
 Clone the repo and run the installation commands, each in a new terminal window.
 
-# Install deps in the project root and in the server directory
+### Import MYSQL database
+import mydatabase.sql
+
+### Install deps in the project root and in the server directory
 npm install
 cd server && npm install
+create .env file
 cd ..
 
-# Run the server
+### Run the server
 npm run server
 
-# New terminal window
+### New terminal window
 npm start
---
+++ mydatabase.sql
@@ -0,0 +1,171 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Gép: 127.0.0.1
+-- Létrehozás ideje: 2017. Feb 09. 22:13
+-- Kiszolgáló verziója: 10.1.13-MariaDB
+-- PHP verzió: 5.6.23
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Adatbázis: `mydatabase`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `company`
+--
+
+CREATE TABLE `company` (
+  `id` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  `company_name` varchar(255) NOT NULL,
+  `tax_number` int(11) NOT NULL,
+  `record_number` int(11) NOT NULL,
+  `phone_number` varchar(30) NOT NULL,
+  `seat` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `finance`
+--
+
+CREATE TABLE `finance` (
+  `id` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  `user_id` int(11) NOT NULL,
+  `closing_balance` int(11) NOT NULL,
+  `phoenix_debit` int(11) NOT NULL,
+  `incoming_oep` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- A tábla adatainak kiíratása `finance`
+--
+
+INSERT INTO `finance` (`id`, `created_at`, `updated_at`, `deleted_at`, `user_id`, `closing_balance`, `phoenix_debit`, `incoming_oep`) VALUES
+(1, '2017-02-09 20:21:59', NULL, NULL, 1, 5000000, 3000000, 2000000);
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `role`
+--
+
+CREATE TABLE `role` (
+  `id` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  `role_name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- A tábla adatainak kiíratása `role`
+--
+
+INSERT INTO `role` (`id`, `created_at`, `updated_at`, `deleted_at`, `role_name`) VALUES
+(1, '2017-02-09 20:19:18', NULL, NULL, 'superadmin'),
+(2, '2017-02-09 20:19:18', NULL, NULL, 'admin'),
+(3, '2017-02-09 20:19:29', NULL, NULL, 'owner'),
+(4, '2017-02-09 20:19:29', NULL, NULL, 'employee');
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  `first_name` varchar(100) NOT NULL,
+  `last_name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `phone_number` varchar(30) NOT NULL,
+  `birthday` timestamp NULL DEFAULT NULL,
+  `title` varchar(50) NOT NULL,
+  `role_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- A tábla adatainak kiíratása `user`
+--
+
+INSERT INTO `user` (`id`, `created_at`, `updated_at`, `deleted_at`, `first_name`, `last_name`, `email`, `password`, `phone_number`, `birthday`, `title`, `role_id`) VALUES
+(1, '2017-02-09 20:21:27', NULL, NULL, 'Elek', 'Teszt', 'teszt.elek@gmail.com', 'teszt123', '+36709525236', '1991-12-31 23:00:00', 'Adatelemző', 4);
+
+--
+-- Indexek a kiírt táblákhoz
+--
+
+--
+-- A tábla indexei `company`
+--
+ALTER TABLE `company`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- A tábla indexei `finance`
+--
+ALTER TABLE `finance`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- A tábla indexei `role`
+--
+ALTER TABLE `role`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- A tábla indexei `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- A kiírt táblák AUTO_INCREMENT értéke
+--
+
+--
+-- AUTO_INCREMENT a táblához `company`
+--
+ALTER TABLE `company`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT a táblához `finance`
+--
+ALTER TABLE `finance`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT a táblához `role`
+--
+ALTER TABLE `role`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT a táblához `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
